7a21570aed982753dfaa8d896f8578bb4d013c31
tannielatte <114336467+tannielatte@users.noreply.github.com>
2023-03-10 17:17:10 +0700

    bye bye bye hiiiiii

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('say hayyyyyy iiiiiiii!');
 });
 
 const PORT = process.env.PORT || 8080;
